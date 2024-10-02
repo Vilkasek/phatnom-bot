eb76bba1e15920c711f4ad7defa0d53d5b26aa7b
Jakub Brandt <jakub.brandt2002@gmail.com>
2024-10-02 10:39:21 +0200

    Created json data loader

---
+++ config-loader.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"encoding/json"
+	"io"
+	"os"
+)
+
+type Config struct {
+	Token string `json:"token"`
+}
+
+func Load_Config(path string) (*Config, error) {
+	file, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	bytes, err := io.ReadAll(file)
+	if err != nil {
+		return nil, err
+	}
+
+	var config Config
+	if err := json.Unmarshal(bytes, &config); err != nil {
+		return nil, err
+	}
+
+	return &config, nil
+}
