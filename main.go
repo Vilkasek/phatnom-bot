5962dc3525502147165dc378b78092a40d465755
Jakub Brandt <jakub.brandt2002@gmail.com>
2024-10-02 11:14:41 +0200

    Created initial bot logic

--- main.go
+++ main.go
@@ -1,4 +1,33 @@
 package main
 
+import (
+	"fmt"
+	"log"
+
+	"github.com/bwmarrin/discordgo"
+)
+
 func main() {
+	config, err := Load_Config("./config.json")
+	if err != nil {
+		log.Fatalf("Nie udało się załadować tokenu: %v", err)
+	}
+
+	bot, err := discordgo.New("Bot " + config.Token)
+	if err != nil {
+		log.Fatalf("Nie udało się zainicjalizować bota: %v", err)
+	}
+
+	err = bot.Open()
+	if err != nil {
+		log.Fatalf("Nie udało się utworzyć websocketu: %v", err)
+	}
+
+	fmt.Println(
+		"Udało sie utworzyć bota oraz połączyć go.\nW celu konserwacji kodu należy bota wyłączyć, ale najpierw poinformuj użytkowników.\nAby wyłączyć bota należy użyć kombinacji klawiszy CTRL + C",
+	)
+
+	defer bot.Close()
+
+	select {}
 }
