771aa68ded7487cf37737f5c21af93a171e059ca
Jakub Brandt <jakub.brandt2002@gmail.com>
2024-10-03 13:06:19 +0200

    Improved and fixed message logger

--- utils/message-logger.go
+++ utils/message-logger.go
@@ -23,22 +23,27 @@ func find_channel_by_id(s *discordgo.Session, guildID string, channelName string
 }
 
 func message_logger(s *discordgo.Session, m *discordgo.MessageCreate) {
+	// Ignorujemy wiadomości wysłane przez samego bota
 	if m.Author.ID == s.State.User.ID {
 		return
 	}
 
-	logChannelName := "logi-wiadomości"
+	// Nazwa kanału, do którego mają trafiać logi wiadomości (zmień na prawdziwą nazwę kanału)
+	logChannelName := "log-wiadomości"
 
+	// Znajdowanie ID kanału na podstawie jego nazwy
 	logChannelID, err := find_channel_by_id(s, m.GuildID, logChannelName)
 	if err != nil {
-		fmt.Println("Nie znaleziono kanału")
+		log.Printf("Nie udało się znaleźć kanału %s: %v", logChannelName, err)
 		return
 	}
 
-	logMessage := "Wiadomość od " + m.Author.Username + " w kanale <#" + m.ChannelID + ">: " + m.Content
+	// Tworzymy wiadomość logującą, zawierającą autora, ID kanału i treść wiadomości
+	logMessage := "Wiadomość od <@" + m.Author.ID + ">" + " w kanale <#" + m.ChannelID + ">: " + m.Content
 
-	_, err = s.ChannelMessage(logChannelID, logMessage)
+	// Wysyłanie wiadomości na kanał logów
+	_, err = s.ChannelMessageSend(logChannelID, logMessage)
 	if err != nil {
-		log.Println("Nie udało się wysłać wiadomości")
+		log.Printf("Nie udało się wysłać wiadomości do kanału logów: %v", err)
 	}
 }
